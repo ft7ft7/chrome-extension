085953556b405a739cb8af6f95f04f47ca4e12d9
ft7ft7 <81555683+ft7ft7@users.noreply.github.com>
2023-11-27 11:14:38 +0800

    Update csdn.js
    pre maxHeight

--- webConfig/csdn.js
+++ webConfig/csdn.js
@@ -1,6 +1,10 @@
 const tagsCode = document.querySelectorAll('#article_content code'),
-	pres = document.querySelectorAll('#article_content pre');
-let codes = Array.from(tagsCode).concat(Array.from(pres));
+	pres = Array.from(document.querySelectorAll('#article_content pre'));
+let codes = Array.from(tagsCode).concat(pres);
 if (codes.length > 0) for (let i = 0; i < codes.length; i++) {
 	codes[i].style.userSelect = 'text';
 }
+
+pres.forEach(v => {
+  v.style.maxHeight = 'unset';
+});
