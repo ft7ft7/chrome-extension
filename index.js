bc5283e62443420ec3d5e94f01b99485d80e8eb1
F T 7 <ft7ft7@outlook.com>
2024-08-18 21:30:58 +0800

    up

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
+const init = {
+	tencent: () => {
+		let target = document.getElementsByClassName('com-markdown-collpase-main');
+		if (target[0]) {
+			target[0].style.maxHeight = 'unset';
+		}
+	},
+	csdn: () => {
+		const tagsCode = document.querySelectorAll('#article_content code'),
+			pres = Array.from(document.querySelectorAll('#article_content pre')),
+			hideBox = document.querySelector('#mainBox main .hide-article-box');
+		if (hideBox) hideBox.style.display = 'none';
+		let codes = Array.from(tagsCode).concat(pres);
+		if (codes.length > 0) for (let i = 0; i < codes.length; i++) {
+			codes[i].style.userSelect = 'text';
+		}
+
+		pres.forEach(v => {
+			v.style.maxHeight = 'unset';
+		});
+	},
+	zhiHu: () => {
+		const arrTagA = document.querySelectorAll('a');
+		for (let i = 0; i < arrTagA.length; i++) {
+			const v = arrTagA[i];
+			if (!/link.zhihu.com/.test(v.href)) continue;
+			let uri = {};
+			v.href.replaceAll(/([^?&]+)=([^?&]+)/g, (s, v, k) => {
+				uri = k;
+				return s;
+			});
+			v.href = decodeURIComponent(uri);
+		}
+		const imgListZhiHu = document.querySelectorAll('img');
+		for (let img of imgListZhiHu) {
+			const src = img.getAttribute('data-actualsrc') || img.getAttribute('data-original');
+			if (/data:image/.test(img.src)) {
+				img.style.display = 'none';
+				continue;
+			}
+			if (src) img.src = src;
+		}
+	},
+	qqNews: () => {
+		const content = document.body.getElementsByClassName('qq_conent')?.[0];
+		if (content) content.style.display = 'block';
+	}
+};
 const config = [{uri: 'cloud.tencent.com', src: 'tencent'},
 	{uri: 'blog.csdn.net', src: 'csdn'},
 	{uri: 'zhuanlan.zhihu.com', src: 'zhiHu'},
@@ -8,7 +56,7 @@ let type = '';
 console.log(urlOrigin, hostname)
 for (let i = 0; i < config.length; i++) {
 	if (new RegExp(config[i].uri).test(hostname)) {
-		import(`./webConfig/${config[i].src}.js`);
+		init[config[i].src]();
 		break;
 	}
 }
